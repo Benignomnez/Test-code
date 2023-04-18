bfda0e2fd432abcbf3251dffc568e65875d37308
[Benigno Martinez] <[test-Code]>
2023-04-18 10:44:51 -0400

    fix function at line 23

--- ex/minimo/minimo.ts
+++ ex/minimo/minimo.ts
@@ -20,18 +20,3 @@
 //Hints: Beware of integer overflow! Use 64-bit Integer.
 
 //Solution
-function miniMaxSum(arr: number[]): void {
-    let min = arr[0];
-    let max = arr[0];
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-        sum += arr[i];
-    }
-    console.log((sum - max) + " " + (sum - min));
-}
